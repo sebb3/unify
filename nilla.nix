778e1d383903f2120d29311497c376ae2fc7932f
quasigod <quasigod-io@proton.me>
2025-04-27 14:57:38 -0400

    simplify nilla.nix

--- nilla.nix
+++ nilla.nix
@@ -1,38 +1,17 @@
 let
   pins = import ./npins;
   nilla = import pins.nilla;
-  nixos = pins.nixos;
 in
 nilla.create (
   { config }:
   {
     includes = [
-      "${nixos}/modules/nixos.nix"
+      "${pins.nixos}/modules/nixos.nix"
       ./unify.nix
     ];
     config = {
-      inputs = {
-        nixpkgs.src = pins.nixpkgs;
-        home-manager.src = pins.home-manager;
-        nilla-utils.src = pins.nilla-utils;
-      };
       modules.unify = ./unify.nix;
       lib = import ./lib.nix { inherit (config) lib; };
-      shells.default = {
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ];
-        shell = let
-          nilla-utils = config.inputs.nilla-utils.result.packages.default.result;
-        in
-          { mkShell, pkgs }:
-          mkShell {
-            packages = [ nilla-utils.${pkgs.system} ];
-          };
-      };
     };
   }
 )
